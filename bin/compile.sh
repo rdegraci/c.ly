c3d0154a53765b8ffa2b47514941320961ebfed8
David Rivera <david.r.rivera193@gmail.com>
2013-11-30 14:05:31 -0500

    moving stuff around

---
+++ bin/compile.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+timestamp="$1"
+syntax="$2"
+
+case $syntax in
+'clike')
+  gcc -x c - -o "$1" 2>&1 && echo -e "\n\n\n" && ./"$1" 2>&1
+  rm "$1" 2>/dev/null
+  ;;
+'python')
+  python
+  ;;
+'javascript')
+  node
+  ;;
+'ruby')
+  ruby
+  ;;
+'haskell') # doesnt work
+  hs
+  ;;
+'r') # doesnt work
+  r
+  ;;
+'less')
+  lessc --no-color -
+  ;;
+esac 
+
